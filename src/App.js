81d409f9f14ea3c01ad1cb7c8343e93e7c409076
giammacarioca <giammattey@gmail.com>
2022-02-19 18:21:13 -0300

    making card choices

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ const cardImages = [
 function App() {
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
 
   // shuffle cards for new game
   const shuffleCards = () => {
@@ -25,7 +27,14 @@ function App() {
     setTurns(0)
   }
 
-  console.log(cards, turns)
+  // console.log(cards, turns)
+
+  // handle a choice
+  const handleChoice = (card) => {
+    console.log(card)
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
   return (
     <div className='App'>
       <h1>Magic Match</h1>
@@ -33,7 +42,7 @@ function App() {
 
       <div className='card-grid'>
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} />
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
         ))}
       </div>
     </div>
